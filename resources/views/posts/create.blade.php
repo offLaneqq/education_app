4d5772c8abf20965df2948727e7387b569bdb477
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-12 18:52:16 +0000

    add text-error after fail validation and some change route

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,21 +1,40 @@
 <x-layout>
     <x-header>Create Post Page</x-header>
     <div class="max-w-2xl mx-auto p-4 bg-slate-200 dark:bg-gray-900 rounded-lg">
-        <form action="/posts" method="POST">
-            @csrf    
-        
+
+        <!-- for fails validation of create post(see PostController->store()) -->
+        <!-- @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <span class="text-red-500">{{ $error }}</span>
+            @endforeach
+        @endif -->
+
+        <form action="{{ route('posts.store') }}" method="POST">
+            @csrf
+
             <div class="mb-6">
-                <label for="default-input" 
+                <label for="default-input"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
                 <input type="text" id="default-input" name="title"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    class="bg-gray-50 border  text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                @error('title')
+                    <span class="text-red-500 text-sm">{{ $message }}</span>
+                @enderror
+
             </div>
 
             <div class="mb-6">
-                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
+                <label for="message"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
                 <textarea id="message" rows="4" name="content"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Write your thoughts here..."></textarea>
+
+                @error('content')
+                    <span class="text-red-500 text-sm">{{ $message }}</span>
+                @enderror
+
             </div>
 
             <div class="mb-6">
