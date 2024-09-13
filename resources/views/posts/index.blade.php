8e6e9266a83714c141747b8d0b35507f0fb494af
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-13 18:59:44 +0000

    use laravel component @auth. Example: if user not authentificated - he don't see the create button

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,12 +4,14 @@
         Main Posts Page
     </x-header>
 
-    <section>
-        <div class="flex justify-end">
-            <a href="{{ route('posts.create') }}"
-                class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Create</a>
-        </div>
-    </section>
+    @auth
+        <section>
+            <div class="flex justify-end">
+                <a href="{{ route('posts.create') }}"
+                    class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Create</a>
+            </div>
+        </section>
+    @endauth
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
