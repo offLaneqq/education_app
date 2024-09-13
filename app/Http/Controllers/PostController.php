7c722c6716748ef9e34bb4237d5e24133fe63070
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-13 18:51:01 +0000

    update destroy function for delete post and redirect to all posts page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -84,8 +84,10 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return to_route('posts.index', ['post' => $post]);
     }
 }
