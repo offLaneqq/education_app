c040ac68cc5d3891ef8b8d5e48bc88cc8fe2a19e
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-13 18:48:45 +0000

    update global routes : add route for register and login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginUserController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,9 @@ Route::get('/', function () {
 
 // All paths for create, update, delete, show, index routes in PostController
 Route::resource('/posts', PostController::class);
+
+Route::get('/register', [RegisterUserController::class, 'register'])->name('register');
+Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
+
+Route::get('/login', [LoginUserController::class, 'login'])->name('login');
+Route::post('/login', [LoginUserController::class, 'store'])->name('login.store');
