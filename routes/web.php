0c112c88042d4f94a1dfed8a9b34c2e83075901d
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-27 18:16:56 +0000

    Add two middleware for auth user and guest. Put route in middleware as need

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// All paths for create, update, delete, show, index routes in PostController
-Route::resource('/posts', PostController::class);
+// Group with middleware for auth user. ONLY AUTH USER CAN DO THAT
+Route::middleware('auth')->group(function () {
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
-Route::get('/register', [RegisterUserController::class, 'register'])->name('register');
-Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
+    Route::post('/logout', [LoginUserController::class, 'logout'])->name('logout');
+});
 
-Route::get('/login', [LoginUserController::class, 'login'])->name('login');
-Route::post('/login', [LoginUserController::class, 'store'])->name('login.store');
+// Group without middleware. Everyone can see all post and open the post
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
-Route::post('/logout', [LoginUserController::class, 'logout'])->name('logout');
+// Test connect custom middleware 'can-view-post' for show function 
+Route::get('/posts/{post}', [PostController::class, 'show'])->middleware('can-view-post')->name('posts.show');
+
+// Group with middlewate for guest. ONLY GUEST CAN REGISTER OR LOGIN
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterUserController::class, 'register'])->name('register');
+    Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
+
+    Route::get('/login', [LoginUserController::class, 'login'])->name('login');
+    Route::post('/login', [LoginUserController::class, 'store'])->name('login.store');
+});
+
+
+
+
+
