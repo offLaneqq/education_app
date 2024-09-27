5dfcb0b7eb580977ca1624d2d425e797603a412a
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-27 19:19:04 +0000

    add user_id field in migration

--- database/migrations/2024_09_04_155625_create_posts_table.php
+++ database/migrations/2024_09_04_155625_create_posts_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->text('content');
             $table->timestamps();
