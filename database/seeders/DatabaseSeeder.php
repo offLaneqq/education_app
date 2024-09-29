d5803b5ac95fc820bbbb6af95d705dd6c4ffd9fc
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-29 10:54:13 +0000

    update seeder for admin account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'is_admin' => true,
+            'password' => bcrypt('password')
         ]);
     }
 }
