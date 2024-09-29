3afa59043c4bd9931c151b52f85171c9167c8563
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-29 13:20:30 +0000

    Remove gare, bcs created middleware for that

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('is-admin', function (User $user) {
-            return $user->is_admin;
-        });
+        // 
     }
 }
