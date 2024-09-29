8cce71bedc1bdcd2ef1c3a3463d5d4501b8177c2
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-29 13:19:53 +0000

    Connect middleware IsAdminMiddleware and use alias to name 'is-admin'

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CanViewPostMiddleware;
+use App\Http\Middleware\IsAdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         // create another name(alias) for middleware
         $middleware->alias(['can-view-post' => CanViewPostMiddleware::class]);
+        $middleware->alias(['is-admin' => IsAdminMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
