3a7311763ee7055312543e7caabc78bfe9c30313
Dmytro Klymchuk <srk90512@gmail.com>
2024-09-29 13:19:12 +0000

    Create admin panel

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,64 @@
+<x-layout>
+
+    <x-header>
+        Main Posts Page
+    </x-header>
+
+    @auth
+        <section>
+            <div class="flex justify-end">
+                <a href="{{ route('posts.create') }}"
+                    class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Create</a>
+            </div>
+        </section>
+    @endauth
+
+
+
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        ID
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Title
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Created At
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $post->id }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $post->title }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $post->created_at}}
+                        </td>
+                        <td class="px-6 py-4">
+                            Edit/Delete
+                        </td>
+                    </tr>
+                @empty
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            No Posts.
+                        </th>
+                    </tr>
+                @endforelse
+
+            </tbody>
+        </table>
+    </div>
+
+</x-layout>
